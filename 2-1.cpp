8d9811c67b889da795f3f03f817ff3bf1a080ed9
E1B12 <167837346+E1B12@users.noreply.github.com>
2024-05-07 00:35:28 +0800

    心得:希望這次的作業有成功繳交，這次的程式作業交作業比寫作業還難，我看上課影片三遍還是不會交作業，之後聽同學說才知道上課有很多介面去哪沒說，還希望下次有要下載其他程式使用的課程提醒帶筆電。
    last

--- 2-1.cpp
+++ 2-1.cpp
@@ -1,7 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main(){
-	int password,n=1;
+void dm();
+void tr(char ch);
+void mult(int n);
+
+int main() {
+    int password,n=1;
 	printf("請輸入4個數字的密碼\n");
 	scanf("%d",&password);
 	while(1){
@@ -14,42 +18,105 @@ int main(){
 			scanf("%d",&password);
 		}
 		else{
-			printf("警告\n");
+			printf("密碼錯誤警告\n");
 			printf("%c",'\a');	//輸出警告聲響
 			return 0;
-		}	
+		}
 	}
 	system("CLS");	// 清除螢幕
-	char a[0];
+    
+    char a;
+
+    do {
+        dm();
+        printf("輸入選項:");
+        scanf(" %c", &a);
+
+        switch (a) {
+            case 'a':
+            case 'A':
+                // Step 3: Draw right angle triangle
+                char ch;
+                printf("請輸入一個'a'到'n'的字元:");
+                scanf(" %c", &ch);
+                tr(ch);
+                break;
+            case 'b':
+            case 'B':
+                // Step 4: Draw multiplication table
+                int n;
+                printf("請輸入一個1至9的整數n:");
+                scanf("%d", &n);
+                mult(n);
+                break;
+            case 'c':
+            case 'C':
+                // Step 5: Continue or exit
+                char yn;
+                printf("Continue? (y/n): ");
+                scanf(" %c", &yn);
+                if (yn == 'n' || yn == 'N') {
+                    printf("結束程式\n");
+                    return 0;
+                } else if (yn != 'y' && yn != 'Y') {
+                    printf("錯誤重新輸入\n");
+                }
+                break;
+            default:
+                printf("錯誤重新輸入\n");
+        }
+    } while (1);
+
+    return 0;
+}
+
+void dm() {
 	printf("----------------------\n");
 	printf("| a. 畫出直角三角形  |\n");
 	printf("| b. 顯示乘法表      |\n");
 	printf("| c. 結束            |\n");
 	printf("----------------------\n");
-	scanf("%s",&a[0]);
-	switch(a[0])
-	{
-		case 'a':
-			printf("%c\n",a[0]);
-			
-			break;
-		case 'b':
-			printf("%c\n",a[0]);
-			
-			break;
-		case 'c':
-			printf("%c\n",a[0]);
-			
-			break;
-		
-		
-		
-		default:
-			break;
-	}
-	
-	
-	
-	printf("eeeee\n");
-	return 0;
+}
+
+void tr(char ch) {
+    if (ch < 'a' || ch > 'n') {
+        printf("輸入字元不在a~n之間，請重新輸入\n");
+        return;
+    }
+    
+
+    printf("\n");
+
+    
+		int i, j;
+  		for (i = 0; i <= ch - 'a'; i++) {
+        	for (j = 0; j < ch - 'a' - i; j++){
+        		printf(" ");
+			}
+        	for (j = 0; j <= i; j++){
+        		printf("%c", ch - i + j);
+			}	
+        	printf("\n");
+  		}
+
+  		system("PAUSE");
+  		system("cls");
+}
+
+void mult(int n) {
+    if (n < 1 || n > 9) {
+        printf("Invalid input. Please enter an integer from 1 to 9.\n");
+        return;
+    }
+
+    printf("\n1*1至%d*%d的乘法表:\n\n",n,n);
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            printf("%dx%d=%d",i,j, i * j);
+        	printf("\t");
+		}
+        printf("\n");
+    }
+  		system("PAUSE");
+  		system("cls");
 }
